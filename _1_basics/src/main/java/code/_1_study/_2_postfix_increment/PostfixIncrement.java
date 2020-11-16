7c9465c875d111c71180f1e5408408bd867a6f65
Ion Alexandra-Gabriela <gabrielaalexandraion@gmail.com>
2020-11-16 21:21:56 +0200

    AlexandraIon

--- _1_basics/src/main/java/code/_1_study/_2_postfix_increment/PostfixIncrement.java
+++ _1_basics/src/main/java/code/_1_study/_2_postfix_increment/PostfixIncrement.java
@@ -4,8 +4,12 @@ public class PostfixIncrement {
   public static void main(String[] args) {
     int x = 0;
     while (x++ < 10) {
+      System.out.println("x=  " + x);
     }
     checkForXValue(x);
+    while (++x < 10) {
+      System.out.println("x=  " + x);
+    }
 
   }
 
