764f5aabca409653e8dbd56df8b9f3d7cac031ba
VladParaschiv <vladparaschiv03@gmail.com>
2020-11-21 16:33:34 +0200

    Update la clasa Main

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,17 +3,47 @@ package code._4_student_effort;
 public class Main {
   public static void main(String[] args) {
 
+    /*
     // First challenge: FizzBuzz
     FirstChallenge firstChallenge = new FirstChallenge();
     firstChallenge.showInformationsAboutChallenge();
+
     firstChallenge.runSimple();
     firstChallenge.runComplex();
+     */
 
+    /*
     // Second challenge:
+    SecondChallenge secondChallenge = new SecondChallenge();
+    secondChallenge.showInformationsAboutChallenge();
 
+    int number = 33;
+    secondChallenge.runSimple(15);
+    secondChallenge.runComplex(101);
+    secondChallenge.runComplex(303);
+    secondChallenge.runComplex(105);
+    secondChallenge.runComplex(10101);
+     */
+
+    /*
     // Third challenge:
+    ThirdChallenge thirdChallenge = new ThirdChallenge();
+    thirdChallenge.showInformationsAboutChallenge();
+
+    int []array = {3,2,-3,-2,3,0};
+    thirdChallenge.run(array);
+    array = new int[]{1, 1, 0, -1, -1};
+    thirdChallenge.run(array);
+    array = new int[]{5,9,-5,7,-5};
+    thirdChallenge.run(array);
+     */
 
     // Fourth challenge:
+    FourthChallenge fourthChallenge = new FourthChallenge();
+    fourthChallenge.showInformationsAboutChallenge();
+
+    int []secondArray = {-1, -1, -1, 2};
+    fourthChallenge.run(secondArray);
 
     //
   }
