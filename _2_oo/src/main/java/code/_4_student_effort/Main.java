6ad1035064ca56d15a31de95aeed2df55576aa02
AndreeaRizea <andreea.rizea1999@gmail.com>
2020-11-23 21:26:43 +0200

    Tema2

--- _2_oo/src/main/java/code/_4_student_effort/Main.java
+++ _2_oo/src/main/java/code/_4_student_effort/Main.java
@@ -2,7 +2,16 @@ package code._4_student_effort;
 
 public class Main {
 
+  public static void codeChallenge1() {
+    Fighter f1 = new Fighter("Doroftei", 100, 25);
+    Fighter f2 = new Fighter("Rocky", 100, 25);
+    BoxingMatch match = new BoxingMatch(f1, f2);
+
+    System.out.println(match.fight());
+  }
+
   public static void main(String[] args) {
     //TODO put your code changes in here
+    codeChallenge1();
   }
 }
