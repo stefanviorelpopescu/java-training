055d7c2d1a48066a03249fb7a378ed918ad95ae2
VoicuIuliana <voicuiuliana486@gmail.com>
2020-11-23 21:28:27 +0200

    Curs 2: Fighter program

--- _2_oo/src/main/java/code/_4_student_effort/Main.java
+++ _2_oo/src/main/java/code/_4_student_effort/Main.java
@@ -2,7 +2,15 @@ package code._4_student_effort;
 
 public class Main {
 
+  public static void codeChallenge1(){
+    Fighter f1 = new Fighter("Doroftei",100,30);
+    Fighter f2 = new Fighter("Rocky",100,35);
+    BoxingMatch match=new BoxingMatch(f1,f2);
+    System.out.println(match.fight());
+  }
+
   public static void main(String[] args) {
-    //TODO put your code changes in here
+    codeChallenge1();
+
   }
 }
