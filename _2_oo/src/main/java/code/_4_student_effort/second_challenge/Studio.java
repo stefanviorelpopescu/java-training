176e95e54124fe56b4d7690940287e34f0bc456a
mihaigabriel18 <mihaicalitescu18@gmail.com>
2020-11-30 18:45:31 +0200

    Something i forgot to add

--- _2_oo/src/main/java/code/_4_student_effort/second_challenge/Studio.java
+++ _2_oo/src/main/java/code/_4_student_effort/second_challenge/Studio.java
@@ -31,4 +31,36 @@ public class Studio {
     public boolean hasTwoMovies() {
         return this.filme.length >= 2;
     }
+
+    public void printStudiosWithActorName(Studio[] studios, String actorName) {
+        for (Studio studio : studios) {
+            found_actor:
+            for (Film film : studio.getFilme())
+                for (Actor actor : film.getActori())
+                    if (actor.getNume().equals(actorName)) {
+                        System.out.print(studio.getNume());
+                        break found_actor;
+                    }
+        }
+        System.out.println();
+    }
+
+    public void printFilmsWithActorAge(Film[] filme, Integer age) {
+        for (Film film : filme) {
+            for (Actor actor : film.getActori())
+                if (actor.getVarsta() >= age) {
+                    System.out.print(film.getNume());
+                    break;
+                }
+        }
+        System.out.println();
+    }
+
+    public void printStudiosWithTwoMovies(Studio[] studios) {
+        for (Studio studio : studios) {
+            if (studio.hasTwoMovies())
+                System.out.print(studio.getNume());
+        }
+        System.out.println();
+    }
 }
