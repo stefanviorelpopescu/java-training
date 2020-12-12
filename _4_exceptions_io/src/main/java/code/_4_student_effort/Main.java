b561a65001abed946ef050fa526c78b279b4f11b
VladParaschiv <vladparaschiv03@gmail.com>
2020-12-12 11:20:31 +0200

    CC4 : challenge 1

--- _4_exceptions_io/src/main/java/code/_4_student_effort/Main.java
+++ _4_exceptions_io/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,38 @@
 package code._4_student_effort;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
+    challenge1();
   }
+
+  public static void challenge1() {
+    Map<String, String> phoneBook = new HashMap<>();
+    BufferedReader file;
+    String[] phone;
+
+    try {
+      file = new BufferedReader(new FileReader("_test_files\\in\\phonebook.txt"));
+      String line;
+
+      while ((line = file.readLine())!= null) {
+        phone = line.split(" ");
+        phoneBook.put(phone[0],phone[1]);
+      }
+
+      System.out.println(phoneBook.get("Abbey"));
+      System.out.println(phoneBook.get("Abdul"));
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
 }
