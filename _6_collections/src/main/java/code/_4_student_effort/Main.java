6962443a0b7ba485f737570419a82b46418c7cbf
VladParaschiv <vladparaschiv03@gmail.com>
2020-12-24 12:09:34 +0200

    Collections: challenge2

--- _6_collections/src/main/java/code/_4_student_effort/Main.java
+++ _6_collections/src/main/java/code/_4_student_effort/Main.java
@@ -2,13 +2,15 @@ package code._4_student_effort;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
-    challenge1();
+    //challenge1();
+    challenge2();
   }
 
   public static void challenge1() {
@@ -28,5 +30,27 @@ public class Main {
 
     toShow.setLength(Math.max(toShow.length()-2, 0));     // delete last " ,"
     System.out.println(toShow.reverse());
+  }
+
+  public static void challenge2() {
+
+    int numberOfLines = 7;
+    List<List<Integer>> arr = new LinkedList<>();
+
+    for (int i=0;i<numberOfLines;i++) {
+      arr.add(new LinkedList<>());
+    }
+
+    for (int line = 0; line<numberOfLines;line++) {
+      for (int i=0;i<=line;i++) {
+        if (line == i || i==0) {
+          arr.get(line).add(1);
+        } else {
+          arr.get(line).add(arr.get(line-1).get(i-1) + arr.get(line-1).get(i));
+        }
+        System.out.print(arr.get(line).get(i) + " ");
+      }
+      System.out.println();
+    }
   }
 }
