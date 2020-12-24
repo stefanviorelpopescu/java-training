951394b19e3bd4d61d1ac9d871d4a42d4519dad2
VladParaschiv <vladparaschiv03@gmail.com>
2020-12-24 11:52:36 +0200

    Collections: challenge1

--- _6_collections/src/main/java/code/_4_student_effort/Main.java
+++ _6_collections/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,32 @@
 package code._4_student_effort;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
+    challenge1();
+  }
+
+  public static void challenge1() {
+
+    List<Integer> arr = Arrays.asList(3, 5, 9, 2, 4, 8, 7, 6); //9,8,7,6 are leaders
+
+    Collections.reverse(arr);             // reverse the array
+    Integer max = Integer.MIN_VALUE;
+    StringBuilder toShow = new StringBuilder();
+
+    for (Integer i : arr) {
+      if (i > max) {
+        max = i;
+        toShow.append(i + " ,");
+      }
+    }
+
+    toShow.setLength(Math.max(toShow.length()-2, 0));     // delete last " ,"
+    System.out.println(toShow.reverse());
   }
 }
