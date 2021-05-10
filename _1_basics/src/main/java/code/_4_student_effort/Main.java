6a22da1aeea8dd4dd142c5f15dae9ca695c65684
AndreiZahan <zahan.andrei95@gmail.com>
2021-05-10 19:45:52 +0300

    commiting our second PR

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -5,5 +5,6 @@ public class Main {
   public static void main(String[] args) {
     //TODO put your code changes in here
     System.out.println("This is a test for our first PR.");
+    System.out.println("Testing out our 2nd commit.");
   }
 }
