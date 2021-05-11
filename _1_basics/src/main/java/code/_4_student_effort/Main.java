133f997eef9267b700d35d19e6acd63287971382
Timeea-Ioana Pitan <timeea.pitan@gmail.com>
2021-05-11 19:31:27 +0300

    Tema 0

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
-    // something changed
-    // 1999
+    //something changed
+    //1999
     System.out.println("Hello there!");
   }
 }
