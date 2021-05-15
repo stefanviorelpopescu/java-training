81585fea6bf51ab3c1bded972ccedef4a0d517fa
Antonia Gherca <gherca.antonia23@gmail.com>
2021-05-15 19:57:31 +0300

    Tema1

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,9 +1,24 @@
 package code._4_student_effort;
+//import java.sql.SQLOutput;
 
 public class Main {
 
-  public static void main(String[] args) {
-    //TODO put your code changes in here
-	  //comentariu
+  public static void main(String[] args)
+  {
+      //Challenge 1
+     Challenge1.display();
+
+      //Challenge 2
+      System.out.println(Challenge2.compute(53));
+      System.out.println(Challenge2.compute2(105));
+
+      //Challenge 3
+      int[] array=new int[] {1,1,0,-1,-1};
+      System.out.println(Challenge3.NrofPairs(array));
+
+      //Challenge4
+      int [] v=new int[] {-1,-1,-1,-1,3,2};
+      System.out.println(Challenge4.Pairof3(v));
+
   }
 }
