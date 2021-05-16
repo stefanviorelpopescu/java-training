c9483cc92e8c38006325f91b6625dbd26bb17176
Bogdan Paval <bogdanpaval97@gmail.com>
2021-05-16 14:22:44 +0300

    Challenge 1-4 uploaded

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -3,9 +3,9 @@ package code._4_student_effort;
 public class Main {
 
   public static void main(String[] args) {
- // Challenge1.run();
+  Challenge1.run();
     System.out.println(Challenge2.compute2(1050640));
-    //Challenge3.run();
-   // Challenge4.run();
+    Challenge3.run();
+    Challenge4.run();
   }
 }
