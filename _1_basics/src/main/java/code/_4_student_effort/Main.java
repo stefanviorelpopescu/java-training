ace06e8de6a500f98765c6724c6ffe4ca8b107f5
bogdancuzmanovici <cuzmanovicibogdan@gmail.com>
2021-05-17 09:43:46 +0300

    Challange 1

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,8 +1,23 @@
 package code._4_student_effort;
 
-public class Main {
+import code._2_challenge._2_foobarqix.FooBarQix;
+
+public class Main extends Challange2{
+  public static void main( String [] args){
+  Challange2 es = new Challange2();
+    System.out.println(es.compute(15));
+
 
 
 
   }
+
+
+  }
+
+
+
+
+
+
 
