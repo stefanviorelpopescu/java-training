10dee4de5c0d456e02e916a2a8b07b7fccd863f4
SRobert1999 <83973606+SRobert1999@users.noreply.github.com>
2021-05-24 12:57:59 +0300

    Tema 2 Stratulat Robert

---
+++ _2_oo/src/main/java/code/_4_student_effort/Actor.java
@@ -0,0 +1,13 @@
+package code._4_student_effort;
+
+public class Actor {
+    public String name;
+    public Integer age;
+    public Award[] awards;
+
+    public Actor(String name, Integer age, Award[] awards){
+        this.name = name;
+        this.age = age;
+        this.awards = awards;
+    }
+}
---
+++ _2_oo/src/main/java/code/_4_student_effort/Movie.java
@@ -0,0 +1,13 @@
+package code._4_student_effort;
+
+public class Movie {
+    public Integer debut;
+    public String name;
+    public Actor[] acttor;
+
+    public Movie(Integer debut, String name, Actor[] acttor){
+        this.debut = debut;
+        this.name = name;
+        this.acttor = acttor;
+    }
+}
