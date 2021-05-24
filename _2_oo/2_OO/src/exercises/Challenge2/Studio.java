fbf259ddd45eb0e611dbb312c00bae873607e1c3
Opris-Ana-30125 <79533946+Opris-Ana-30125@users.noreply.github.com>
2021-05-24 14:19:53 +0300

    2_OOTema_2

---
+++ _2_oo/2_OO/src/exercises/Challenge2/Studio.java
@@ -0,0 +1,11 @@
+package exercises.Challenge2;
+
+public class Studio {
+  public String nume;
+  public Film[] filme;
+
+  public Studio(String nume, Film[] filme) {
+    this.nume = nume;
+    this.filme = filme;
+  }
+}
