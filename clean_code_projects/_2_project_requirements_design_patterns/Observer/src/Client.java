e49edfb7f487dd66a9d5dc6ec2122e3b7f2c0693
Opris-Ana-30125 <79533946+Opris-Ana-30125@users.noreply.github.com>
2021-07-17 22:38:29 +0300

    Design Patterns

---
+++ clean_code_projects/_2_project_requirements_design_patterns/Observer/src/Client.java
@@ -0,0 +1,17 @@
+public class Client {
+    public static void main(String[] args) throws InterruptedException {
+        Alarm fireAlarm = new Alarm();
+        AlarmMonitor monitor = new AlarmMonitor();
+        AlarmController controller = new AlarmController();
+
+        fireAlarm.register(monitor);
+        fireAlarm.register(controller);
+        fireAlarm.startAlarm();
+
+        Thread.sleep(500);
+        fireAlarm.stopAlarm();
+        fireAlarm.notifyAllObservers(controller);
+
+
+    }
+}
